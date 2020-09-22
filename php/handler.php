cd86a1664e63f1fd6126857028ae43e62bac9060
rabbitdd <thelordstig@gmail.combahsbbs-fdfdfdfdfvv>
2020-09-22 10:07:12 +0300

    version 1.0

--- php/handler.php
+++ php/handler.php
@@ -3,40 +3,72 @@ session_start();
 date_default_timezone_set("Europe/Moscow");
 $current_time = date("H:m:s");
 $start = microtime(true);
+$box = array(-4, -3, -2, -1, 0, 1, 2, 3, 4);
+$rad = array(1, 1.5, 2, 2.5, 3);
 $x = ($_GET['check_box_group']);
-$y = ($_GET['y']);
+$y = "2.23232";//($_GET['y']);
 $r = ($_GET['radio_group']);
 $ans = "";
+$chek = 0;
 
-if ($x > 0 && $y > 0) {
-    $ans = "NO";
-} else if ($x <= 0 && $y >= 0) {
-    //echo ('2');
-    if ($x * $x + $y * $y <= $r * $r)
-        $ans = "YES";
-    else
-        $ans = "NO";
-} else if ($x < 0 && $y < 0) {
-    if (($y - 0.00001 >= (-1 * ($r / 2)) && ($x - 0.00001 >= -1 * $r)))
-        $ans = "YES";
-    else
-        $ans = "NO";
-} else {
-    //echo ('4');
-    if ((2 * $x * $r - $y * $r - $r ** 2) <= 0) {
-        $ans = "YES";
+function checkY($y) {
+    if (strrpos($y, '.') != -1) {
+        $idx = strrpos($y, '.');
+        $ceil = substr($y, 0, $idx);
+        $post = substr($y, $idx + 1);
+        if (abs($ceil) <= 3) {
+            return true;
+        } else if (preg_match('/[1-9]/', $post)) {
+            return false;
+        }
+
     } else {
-        $ans = "NO";
+        return abs($y) <= 3;
     }
 }
 
-$time = microtime(true) - $start;
-$res_table = array((int) $x, (float) $y, (float) $r, $current_time, $time, $ans);
+function valid($x, $y, $r, $start, $current_time, $box, $rad) {
 
-if (!isset($_SESSION['history'])) {
-    $_SESSION['history'] = array();
+
+    if (is_numeric($x) && is_numeric($y) && is_numeric($r) && checkY($y)&& (abs($y) <= 3) && array_search($x, $box) && array_search($r, $rad)) {
+        if ($x > 0 && $y > 0) {
+            $ans = "NO";
+        } else if ($x <= 0 && $y >= 0) {
+            //echo ('2');
+            if ($x * $x + $y * $y <= $r * $r)
+                $ans = "YES";
+            else
+                $ans = "NO";
+        } else if ($x < 0 && $y < 0) {
+            if (($y - 0.00001 >= (-1 * ($r / 2)) && ($x - 0.00001 >= -1 * $r)))
+                $ans = "YES";
+            else
+                $ans = "NO";
+        } else {
+            //echo ('4');
+            if ((2 * $x * $r - $y * $r - $r ** 2) <= 0) {
+                $ans = "YES";
+            } else {
+                $ans = "NO";
+            }
+        }
+
+        $time = microtime(true) - $start;
+        $res_table = array((int) $x, (float)$y, (float) $r, $current_time, $time, $ans);
+
+        if (!isset($_SESSION['history'])) {
+            $_SESSION['history'] = array();
+        }
+        array_push($_SESSION['history'], $res_table);
+
+
+    }
+    //array_push($_SESSION['history'], $res_table);
 }
-
-array_push($_SESSION['history'], $res_table);
+valid($x, $y, $r, $start, $current_time, $box, $rad);
 include "table.php";
+
+
+
+
 
